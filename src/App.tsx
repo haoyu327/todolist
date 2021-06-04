7189c8f6bf862e31b4669da99e4a88508f2c1730
haoyuhsu <75198635+haoyu327@users.noreply.github.com>
2021-06-04 17:48:31 +0800

    prettier format modify

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,69 @@
-import React, { useState } from 'react'
-import './App.css'
-import { TodoForm } from './components/todoForm'
-import { TodoList } from './components/TodoList'
-import { Typography, Card, AppBar, Toolbar } from '@material-ui/core'
+import React, { useState } from "react";
+import "./App.css";
+import { TodoForm } from "./components/todoForm";
+import { TodoList } from "./components/TodoList";
+import { Typography, Card, AppBar, Toolbar } from "@material-ui/core";
 import {
   AddTodo,
   DeleteTodo,
   saveTodo,
   Todo,
   ToggleComplete,
-} from './components/todoModel'
+} from "./components/todoModel";
 
 const styles = {
   bar: {
-    backgroundColor: '#274472',
+    backgroundColor: "#274472",
   },
-}
-let index = 1
+};
+let index = 1;
 
 function App() {
-  let [todos, setTodos] = useState<Array<Todo>>([])
+  let [todos, setTodos] = useState<Array<Todo>>([]);
 
   const toggleComplete: ToggleComplete = (selectedTodo) => {
     const updatedTodos = todos.map((todo) => {
       if (todo === selectedTodo) {
-        return { ...todo, complete: !todo.complete }
+        return { ...todo, complete: !todo.complete };
       }
-      return todo
-    })
-    setTodos(updatedTodos)
-  }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
   const deleteTodo: DeleteTodo = (selectedTodo) => {
-    todos = todos.filter((x) => x.id !== selectedTodo.id)
-    setTodos(todos)
-    console.log(todos)
-  }
+    todos = todos.filter((x) => x.id !== selectedTodo.id);
+    setTodos(todos);
+    console.log(todos);
+  };
   const editTodo: DeleteTodo = (selectedTodo) => {
     const updatedTodos = todos.map((todo) => {
       if (todo === selectedTodo && todo.isedit === false) {
-        return { ...todo, isedit: true }
+        return { ...todo, isedit: true };
       } else if (todo === selectedTodo && todo.isedit === true) {
-        return { ...todo, isedit: false }
+        return { ...todo, isedit: false };
       }
-      return todo
-    })
-    setTodos(updatedTodos)
-  }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
   const saveTodo: saveTodo = (text, selectedTodo) => {
     const updatedTodos = todos.map((todo) => {
       if (todo === selectedTodo) {
-        return { ...todo, text: text }
+        return { ...todo, text: text };
       }
-      return todo
-    })
-    setTodos(updatedTodos)
-  }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
   const addTodo: AddTodo = (newTodo) => {
-    if (newTodo !== '') {
+    if (newTodo !== "") {
       setTodos([
         ...todos,
         { id: index, text: newTodo, complete: false, isedit: false },
-      ])
-      index = index + 1
+      ]);
+      index = index + 1;
     }
-  }
+  };
 
   return (
     <div>
@@ -86,7 +86,7 @@ function App() {
         />
       </Card>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
